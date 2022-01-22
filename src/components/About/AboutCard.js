9a3b505b16438f6152d763fccf70f01032dc470f
HIREN lalani <72781029+hiren14@users.noreply.github.com>
2022-01-22 17:28:39 +0530

    Update AboutCard.js

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -9,7 +9,7 @@ function AboutCard() {
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
             Hi Everyone, I am <span className="purple">HIREN LALANI </span>
-            from <span className="purple"> Bhubaneswar, India.</span>
+            from <span className="purple"> Surat,Gujarat, India.</span>
             <br />I am a UNDERGUDATE at Goverment engineering college Patan in computer science
             Mesra.
             <br />
